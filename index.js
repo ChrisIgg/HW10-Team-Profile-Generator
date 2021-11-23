a9b7eddfe34580c3ae89b9f063611f4d9fc1506e
Chris Insignares <insignaresc115@gmail.com>
2021-11-22 23:00:38 -0500

    update for push

--- index.js
+++ index.js
@@ -16,32 +16,69 @@ app.use(express.static("public"));
 const teamArray = [];
 
 function getEmployeeInfo() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "Enter employee's name: ",
-      name: "employeeName",
-    },
-    {
-      type: "input",
-      message: "Enter employee's ID: ",
-      name: "employeeID",
-    },
-    {
-      type: "input",
-      message: "Enter employee's email: ",
-      name: "employeeEmail",
-    },
-    {
-      type: "list",
-      message: "Select employee's position: ",
-      choices: ["Manager", "Engineer", "Intern"],
-      name: "employeeJob",
-    },
-  ]);
-  // .then(function ({ employeeName, employeeID, employeeEmail, employeeJob}) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter employee's name: ",
+        name: "employeeName",
+      },
+      {
+        type: "input",
+        message: "Enter employee's ID: ",
+        name: "employeeID",
+      },
+      {
+        type: "input",
+        message: "Enter employee's email: ",
+        name: "employeeEmail",
+      },
+      {
+        type: "list",
+        message: "Select employee's position: ",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "employeeJob",
+      },
+    ])
+    .then(function ({ employeeName, employeeID, employeeEmail, employeeJob }) {
+      let jobType = "";
 
-  // }
+      if (employeeJob === "Manager") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter you Office Number",
+            name: "officeNumber",
+          },
+          {
+            type: "list",
+            message: "Do you want to add an employee?",
+            choices: ["Yes", "No"],
+            name: "addEmployee",
+          },
+        ]);
+        // .then(function ({ officeNumber, addEmployee}) {
+        //     let newEmployee;
+
+        // })
+        jobType = "Office Number";
+      } else if (employeeJob === "Engineer") {
+        jobType = "Github Username";
+      } else if (employeeJob === "Intern") {
+        jobType = "School";
+      }
+      inquirer.prompt([
+        {
+          type: "input",
+          message: `Enter your ${jobType}: `,
+          name: "employeeInfo",
+        },
+      ]);
+    });
 }
 
 getEmployeeInfo();
+
+function runManager() {
+  inquirer.prompt;
+}
