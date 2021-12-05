958baed55aa952c9374e40cb0cd3affb607ea88c
Chris Insignares <insignaresc115@gmail.com>
2021-12-05 17:11:26 -0500

    utilized manager to add managers to a team array

--- index.js
+++ index.js
@@ -4,76 +4,45 @@ const Employee = require("./team/employee");
 const Manager = require("./team/manager");
 const Engineer = require("./team/engineer");
 const Intern = require("./team/intern");
-const PORT = process.env.Port || 3001;
-const express = require("express");
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
 
 const teamArray = [];
+console.log("empty?", teamArray);
 
 function getEmployeeInfo() {
   inquirer
     .prompt([
       {
         type: "input",
-        message: "Enter employee's name: ",
+        message: "Enter manager's name: ",
         name: "employeeName",
       },
       {
         type: "input",
-        message: "Enter employee's ID: ",
+        message: "Enter manager's ID: ",
         name: "employeeID",
       },
       {
         type: "input",
-        message: "Enter employee's email: ",
+        message: "Enter manager's email: ",
         name: "employeeEmail",
       },
       {
-        type: "list",
-        message: "Select employee's position: ",
-        choices: ["Manager", "Engineer", "Intern"],
-        name: "employeeJob",
+        type: "input",
+        message: "Enter manager's office number: ",
+        name: "officeNumber",
       },
+      // new Manager = (employeeID, employeeName, emplyeeEmail, officeNumer)
     ])
-    .then(function ({ employeeName, employeeID, employeeEmail, employeeJob }) {
-      let jobType = "";
-
-      if (employeeJob === "Manager") {
-        inquirer.prompt([
-          {
-            type: "input",
-            message: "Enter you Office Number",
-            name: "officeNumber",
-          },
-          {
-            type: "list",
-            message: "Do you want to add an employee?",
-            choices: ["Yes", "No"],
-            name: "addEmployee",
-          },
-        ]);
-        // .then(function ({ officeNumber, addEmployee}) {
-        //     let newEmployee;
-
-        // })
-        jobType = "Office Number";
-      } else if (employeeJob === "Engineer") {
-        jobType = "Github Username";
-      } else if (employeeJob === "Intern") {
-        jobType = "School";
-      }
-      inquirer.prompt([
-        {
-          type: "input",
-          message: `Enter your ${jobType}: `,
-          name: "employeeInfo",
-        },
-      ]);
+    .then(function ({ employeeName, employeeID, employeeEmail, officeNumber }) {
+      const newManager = new Manager(
+        employeeID,
+        employeeName,
+        employeeEmail,
+        officeNumber
+      );
+      console.log("new manager: ", newManager);
+      teamArray.push(newManager);
+      console.log("pushed array: ", teamArray);
     });
 }
 
@@ -81,4 +50,20 @@ getEmployeeInfo();
 
 function runManager() {
   inquirer.prompt;
+}
+
+function verifyManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter you Office Number",
+      name: "officeNumber",
+    },
+    {
+      type: "list",
+      message: "Do you want to add an employee?",
+      choices: ["Yes", "No"],
+      name: "addEmployee",
+    },
+  ]);
 }
